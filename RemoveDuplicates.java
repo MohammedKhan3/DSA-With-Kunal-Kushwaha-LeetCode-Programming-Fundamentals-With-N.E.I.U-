27d179d797e1e2c3aab089f4a56d511dffe9298b
Mohammed Fawzaan Khan <159211205+MohammedKhan3@users.noreply.github.com>
2024-02-18 22:24:52 -0600

    Create RemoveDuplicates.java

---
+++ RemoveDuplicates.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int counter = 0;
+        for(int i = 0; i<nums.length;i++){
+            if(i < nums.length-1 && nums[i]==nums[i+1]){
+                continue;
+                
+            }else{
+                nums[counter] = nums[i];
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
